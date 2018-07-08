cb04809c2518e74917280e0dc409e2b8df313e62
Marcela Fontes Lima Guerra <marcela.guerra@gmail.com>
2018-07-08 20:31:20 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,13 +2,18 @@
 
 def display_board(board)
 
+
 end
 
 def valid_move?(board, position)
 
 end
 
-def position_taken?(board, positio)
+def position_taken?(board, position)
+
+end
+
+def move(board, position, element="X")
 
 
 def turn
